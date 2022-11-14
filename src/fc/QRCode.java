d3462bc7c0781d43613648a4ca06fe4cf72ca5db
bastamar <marion.bastard-rosset@etu.univ-grenoble-alpes.fr>
2022-11-14 15:05:33 +0100

    Co-authored-by: Berenguier <Berenguier@users.noreply.github.com> Co-authored-by: bouchraanni <bouchraanni@users.noreply.github.com> Co-authored-by: Popolato <Popolato@users.noreply.github.com>

---
+++ src/fc/QRCode.java
@@ -0,0 +1,12 @@
+package fc;
+public class QRCode extends Support {
+
+	QRCode(Film f) {
+		super(f);
+		//TODO Auto-generated constructor stub
+	}
+    
+    void sortir_support(){
+        System.out.println("QRCode sorti");
+    }
+}
