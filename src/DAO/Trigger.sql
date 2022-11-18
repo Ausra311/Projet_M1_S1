085e22ea53cc43b5b0b8cc250279f703c34c00dd
bastamar <marion.bastard-rosset@etu.univ-grenoble-alpes.fr>
2022-11-18 14:30:38 +0100

    commit

--- src/DAO/Trigger.sql
+++ src/DAO/Trigger.sql
@@ -3,9 +3,11 @@ Before delete or insert on AbonneEnfant
 for each row
 Begin 
 	if (deleting) then
-		update AbonneParent Set nbEnfant = nbEnfant-1 where noClient = :old.noParent;
+		update AbonneParent Set nbEnfant = nbEnfant-1 
+							where noClient = :old.noParent;
 	else
-		update LesAnimaux Set nbEnfant = nbEnfant+1 where noClient = :old.noParent;
+		update AbonneParent Set nbEnfant = nbEnfant+1 
+							where noClient = :new.noParent;
 	end if;
 End;
 /
@@ -14,8 +16,21 @@ Create or replace trigger LocationFilm
 Before insert on Location
 for each row
 Begin 
-    update Film Set nbLoue = nbLoue+1 where noFilm = (distinct noFilm from Support where noSupport = :old.noParent);
-    
+	if ((Select Count(noClient) from Location 
+							where retourne = 0 and noSupport = :new.noSupport 
+							group by noSupport) != 0) then
+		raise_application_error(-20001, 'Support indisponible')
+	end if
+
+    update Film Set nbLoue = nbLoue+1 
+				where noFilm = (distinct noFilm from Support 
+												where noSupport = :new.noSupport);
+	
+    if ((select typeCarte from Client 
+						where noClient = :new.noClient) = 'Sans Carte') then
+		update CarteBancaire set dateUtil = :new.dateEmprunt 
+							where noClient = :new.noClient;
+	end if;
 End;
 /
 
