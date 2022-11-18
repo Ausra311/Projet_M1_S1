bbce1a814c83d101cab6cdd5b9d802f8818b48a5
Berenguier <95769269+Berenguier@users.noreply.github.com>
2022-11-18 18:32:33 +0100

    debut ihm
    accueil connexion rendre

---
+++ Connexion.java
@@ -0,0 +1,48 @@
+package ihm;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.*;
+
+
+public class Connexion extends JPanel{
+
+    Connexion(JFrame f){
+        JFrame Fenetre = f;
+        setLayout(new BorderLayout());
+
+        JPanel sud = new JPanel();
+        JPanel centre = new JPanel();
+        JButton Inserer_carte = new JButton("Inserer carte");
+        JButton Retour = new JButton("Retour");
+
+        // Placement
+        JPanel haut = new JPanel(); 
+        haut.setPreferredSize(new Dimension(600,400));
+        
+        //Boutons
+        Inserer_carte.setPreferredSize(new Dimension(400,80));
+        Inserer_carte.setFont(new Font("Arial",Font.BOLD,25));
+
+        Retour.setPreferredSize(new Dimension(110,50));
+        Retour.addActionListener(new ActionListener()
+        {
+        public void actionPerformed(ActionEvent e){
+            Fenetre.setContentPane(new Accueil(Fenetre));
+            Fenetre.revalidate();
+            }
+        });
+        
+        centre.add(Inserer_carte);
+        sud.setLayout(new BorderLayout());
+        sud.add(Retour,BorderLayout.WEST);
+
+        //Panel principal
+        add(centre,BorderLayout.CENTER);
+        add(sud,BorderLayout.SOUTH);
+        add(haut,BorderLayout.NORTH);
+
+    }
+}
