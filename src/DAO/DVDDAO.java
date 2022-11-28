42ecab8aecfee07de0b9c05de1091cf137a6637b
bastamar <marion.bastard-rosset@etu.univ-grenoble-alpes.fr>
2022-11-28 15:47:43 +0100

    commit

--- src/DAO/DVDDAO.java
+++ src/DAO/DVDDAO.java
@@ -1,8 +1,13 @@
 package DAO;
 
 import fc.DVD;
+import fc.Film;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Vector;
 
 public class DVDDAO extends DAO<DVD>{
     
@@ -17,8 +22,14 @@ public class DVDDAO extends DAO<DVD>{
     }
 
     @Override
-    public DVD read(Object obj) throws SQLException {
-        // TODO Auto-generated method stub
+    public Vector<DVD> read(Object obj) throws SQLException {
+           DVD dvd = null;
+           try (PreparedStatement Resul = conn.prepareStatement("select distinct d.noSupport , d.etat , d.emplacement from DVD d , ((Select d.noSupport from DVD d, Support s where s.nofilm = ? and s.nosupport = d.nosupport) MINUS (Select l.noSupport from Location l, Support s where s.nofilm = ? and s.nosupport = l.nosupport) k where k.noSupport = d.noSupport)")){   
+                 Resul.setInt(1,((Film)obj).get_id());
+                 Resul.setInt(2,((Film)obj).get_id());
+                 ResultSet resultSet = Resul.executeQuery();
+                 
+           }
         return null;
     }
 
--- src/DAO/FilmDAO.java
+++ src/DAO/FilmDAO.java
@@ -1,8 +1,12 @@
 package DAO;
 
+import fc.Client;
 import fc.Film;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Vector;
 
 public class FilmDAO extends DAO<Film> {
 
@@ -17,8 +21,76 @@ public class FilmDAO extends DAO<Film> {
     }
 
     @Override
-    public Film read(Object obj) throws SQLException {
-        // TODO Auto-generated method stub
+    public Vector<Film> read(Object obj) throws SQLException {
+        Client c = (Client)obj;
+        Vector<Film> liste_film = new Vector<Film>();
+
+        try (PreparedStatement typeClient = conn.prepareStatement("SELECT typeClient FROM Client WHERE noClient = ?)");
+        PreparedStatement noFilm = conn.prepareStatement("SELECT noFilm FROM FILM)"); 
+        PreparedStatement noFilmEnfant = conn.prepareStatement("(SELECT noFilm FROM FILM) MINUS (SELECT DISTINCT g.noFilm FROM Restriction r, Genre g where r.genre = g.genre and r.noclient = ?");
+        PreparedStatement info_Film = conn.prepareStatement("SELECT * FROM Film WHERE noFilm = ?");
+        PreparedStatement genre_Film = conn.prepareStatement("SELECT genre FROM Genre WHERE noFilm = ?");
+        PreparedStatement acteur_Film = conn.prepareStatement("SELECT nomActeur, prenomActeur FROM Acteuur WHERE noFilm = ?");){
+        
+        typeClient.setInt(1, obj.get_id());
+        ResultSet res1 = typeClient.executeQuery();
+        String Type;
+
+        if (res1.next()) {
+            Type = res1.getString(1);
+        }
+
+        ResultSet Liste_noFilm;
+
+        if (Type == "Abonne Enfant"){
+            noFilmEnfant.setInt(1, obj.get_id());
+            Liste_noFilm = noFilmEnfant.executeQuery();
+        }else{
+            Liste_noFilm = noFilm.executeQuery();
+        }
+
+        while (Liste_noFilm.next()) {
+
+            Vector<String> Acteur = new Vector<String>();
+            Vector<String> Genre = new Vector<String>();
+
+            
+
+            Film f = new Film(0, Type, Type, null, Type, null, 0, 0);
+            liste_film.add(f);
+		}
+        
+
+        (PreparedStatement lesEmployes = conn.prepareStatement("SELECT NOME, ADRESSE FROM LESEMPLOYES WHERE NOME = ?");
+		     PreparedStatement lesGardiens = conn.prepareStatement("SELECT NOCAGE FROM LESGARDIENS WHERE NOME = ?");
+		     PreparedStatement lesSpecialites = conn.prepareStatement("SELECT FONCTION_CAGE FROM LESSPECIALITES WHERE NOME = ?")) {
+			lesEmployes.setString(1, (String)g);
+			ResultSet resultSet = lesEmployes.executeQuery();
+
+			gardien = new Gardien();
+			if (resultSet.next()) {
+				gardien.setNomE(resultSet.getString(1));
+				gardien.setAdresse(resultSet.getString(2));
+			}
+
+			lesGardiens.setString(1, (String)g);
+			resultSet = lesGardiens.executeQuery();
+			while (resultSet.next()) {
+				Cage c = new Cage();
+				c.setNoCage(resultSet.getInt(1));
+				gardien.addCage(c);
+			}
+
+			lesSpecialites.setString(1, (String)g);
+			resultSet = lesSpecialites.executeQuery();
+			while (resultSet.next()) {
+				gardien.addSpecialites(resultSet.getString(1));
+			}
+            
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
         return null;
     }
 
